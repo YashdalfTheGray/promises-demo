052057fa717a193397fb59e428697b0a669b9d0a
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-11-05 21:00:20 -0600

    added the base app

---
+++ public/app.js
@@ -0,0 +1,37 @@
+/* global angular */
+/* global _ */
+
+angular.module('promisesDemo', 
+    [
+        'ui.router',
+        'ngAnimate',
+        'ngMaterial',
+        'firebase'
+    ]
+)
+.config(
+    [
+        '$urlRouterProvider', '$stateProvider', '$mdThemingProvider',
+        function($urlRouterProvider, $stateProvider, $mdThemingProvider) {
+            "use strict";
+
+            /*
+            $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'home/home.tpl.html',
+                controller: 'HomeCtrl as ctrl'
+            });
+            $urlRouterProvider.otherwise('/');
+            */
+
+            $mdThemingProvider.theme('default')
+                .primaryPalette('blue-grey')
+                .accentPalette('amber',{
+                    'default': 'A400',
+                    'hue-1': 'A700'
+                })
+                .warnPalette('red');
+        }
+    ]
+);
