a233ffe29caac31214ff0294f6cdc157e086dc3d
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-11-08 18:28:25 -0600

    added a page for simple promise handling

---
+++ public/simplePromise/simplePromise.js
@@ -0,0 +1,12 @@
+/* global angular */
+
+angular.module('promisesDemo')
+.controller('SimplePromiseCtrl',
+    [
+        function() {
+            "use strict";
+
+            var vm = this;
+        }
+    ]
+);
